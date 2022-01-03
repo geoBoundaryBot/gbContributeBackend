67eada015d9b1e0fbee25508d810d931127fc097
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-01-03 23:59:36 +0100

    Added heroku docs, updated some configs/settings

--- core/settings.py
+++ core/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gbcontributebackend.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'geoboundary-contributions.herokuapp.com']
 
 
 # Application definition
